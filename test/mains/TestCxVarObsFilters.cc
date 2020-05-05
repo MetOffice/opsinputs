6c9c8fdf42cf6182963f72857231c6681220b1f1
Wojciech Smigaj <wojciech.smigaj@metoffice.gov.uk>
2020-05-05 14:49:37 +0100

    Added a (stub) unit test.

---
+++ test/mains/TestCxVarObsFilters.cc
@@ -0,0 +1,18 @@
+/*
+ * (C) Copyright 2020 UCAR
+ * 
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
+ */
+
+#include "../test/ufo/ObsFilters.h"
+#include "cxvarobs/instantiateObsFilterFactory.h"
+#include "oops/runs/Run.h"
+#include "ufo/UfoTrait.h"
+
+int main(int argc,  char ** argv) {
+  oops::Run run(argc, argv);
+  cxvarobs::instantiateObsFilterFactory<ufo::UfoTrait>();
+  ufo::test::ObsFilters tests;
+  return run.execute(tests);
+}
