671007ef08be3a319bd728ae19771415dd9328aa
Wojciech Smigaj <wojciech.smigaj@metoffice.gov.uk>
2020-06-12 14:10:11 +0100

    Created a simple CMake project with two tests

---
+++ testapp1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  std::cout << "This is a successful test (stdout).\n";
+  std::cerr << "This is a successful test (stderr).\n";
+  return 0;
+}
---
+++ testapp2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  std::cout << "This is a failing test (stdout).\n";
+  std::cerr << "This is a failing test (stderr).\n";
+  return 1;
+}
