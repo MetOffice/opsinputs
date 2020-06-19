97f7a943c0b506b33f34deafc74e50b1f2be49e7
Wojciech Smigaj <wojciech.smigaj@metoffice.gov.uk>
2020-06-19 10:58:08 +0100

    Moved common implementations of ObsSpace wrappers for int- and float-valued variables into template functions.

--- src/opsinputs/opsinputs_obsdatavector_f.cc
+++ src/opsinputs/opsinputs_obsdatavector_f.cc
@@ -18,48 +18,74 @@
 
 namespace opsinputs {
 
-int opsinputs_obsdatavector_int_nlocs_f(const ioda::ObsDataVector<int> &vec) {
+namespace {
+
+template <typename T>
+int opsinputs_obsdatavector_nlocs_f(const ioda::ObsDataVector<T> &vec) {
   return vec.nlocs();
+}
+
+template <typename T>
+const oops::Variables * opsinputs_obsdatavector_varnames_f(
+    const ioda::ObsDataVector<T> &vec) {
+  return &vec.varnames();
+}
+
+template <typename T>
+bool opsinputs_obsdatavector_has_f(const ioda::ObsDataVector<T> &vec,
+                                   const char *variable) {
+  return vec.has(variable);
+}
+
+template <typename T>
+void opsinputs_obsdatavector_get_f(const ioda::ObsDataVector<T> &vec,
+                                       const char *variable,
+                                       const std::size_t &length, T* data) {
+  const ioda::ObsDataRow<T> &row = vec[variable];
+  ASSERT(length >= row.size());
+  std::copy(row.begin(), row.end(), data);
+}
+
+}  // namespace
+
+int opsinputs_obsdatavector_int_nlocs_f(const ioda::ObsDataVector<int> &vec) {
+  return opsinputs_obsdatavector_nlocs_f(vec);
 }
 
 const oops::Variables * opsinputs_obsdatavector_int_varnames_f(
     const ioda::ObsDataVector<int> &vec) {
-  return &vec.varnames();
+  return opsinputs_obsdatavector_varnames_f(vec);
 }
 
 bool opsinputs_obsdatavector_int_has_f(const ioda::ObsDataVector<int> &vec,
-                                      const char *variable) {
-  return vec.has(variable);
+                                       const char *variable) {
+  return opsinputs_obsdatavector_has_f(vec, variable);
 }
 
 void opsinputs_obsdatavector_int_get_f(const ioda::ObsDataVector<int> &vec,
-                                      const char *variable,
-                                      const std::size_t &length, int* data) {
-  const ioda::ObsDataRow<int> &row = vec[variable];
-  ASSERT(length >= row.size());
-  std::copy(row.begin(), row.end(), data);
+                                       const char *variable,
+                                       const std::size_t &length, int* data) {
+  opsinputs_obsdatavector_get_f(vec, variable, length, data);
 }
 
 int opsinputs_obsdatavector_float_nlocs_f(const ioda::ObsDataVector<float> &vec) {
-  return vec.nlocs();
+  return opsinputs_obsdatavector_nlocs_f(vec);
 }
 
 const oops::Variables * opsinputs_obsdatavector_float_varnames_f(
     const ioda::ObsDataVector<float> &vec) {
-  return &vec.varnames();
+  return opsinputs_obsdatavector_varnames_f(vec);
 }
 
 bool opsinputs_obsdatavector_float_has_f(const ioda::ObsDataVector<float> &vec,
-                                        const char *variable) {
-  return vec.has(variable);
+                                         const char *variable) {
+  return opsinputs_obsdatavector_has_f(vec, variable);
 }
 
 void opsinputs_obsdatavector_float_get_f(const ioda::ObsDataVector<float> &vec,
-                                        const char *variable,
-                                        const std::size_t &length, float* data) {
-  const ioda::ObsDataRow<float> &row = vec[variable];
-  ASSERT(length >= row.size());
-  std::copy(row.begin(), row.end(), data);
+                                         const char *variable,
+                                         const std::size_t &length, float* data) {
+  opsinputs_obsdatavector_get_f(vec, variable, length, data);
 }
 
 }  // namespace opsinputs
