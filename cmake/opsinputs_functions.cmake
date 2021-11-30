b43eb92d47a32e36d07c223d7298f496c01bad74
Michael Cooke <48374999+mikecooke77@users.noreply.github.com>
2021-11-30 16:51:06 +0000

    Add the operational namelists and some ctests (#51)
    * Update the surface pressure names
    
    * Add the default cx and varobs namelists.  test to follow.
    
    * Cx test for full atms nl added
    
    * Add a ctest for the full atms varobs namelist
    
    * Update tests and data for additional variables in cx and varobs
    
    * Update test file generate script
    
    * Updates following @ctgh review.
    
    * Remove duplicate cx number

---
+++ cmake/opsinputs_functions.cmake
@@ -0,0 +1,19 @@
+# Syntax: CREATE_SYMLINKS(src dst)
+#
+# Arguments:
+# - src: Source folder
+# - dst: Destination folder
+#
+# Creates symbolic links in the destination folder pointing to the specified files in the source
+# folder, preserving subfolder hierarchy.
+function(CREATE_SYMLINKS src dst)
+  file(MAKE_DIRECTORY ${dst})
+  foreach (FILENAME ${ARGN})
+    get_filename_component(absolute_subdir ${src}/${FILENAME} DIRECTORY)
+    file(RELATIVE_PATH relative_subdir ${src} ${absolute_subdir})
+    file(MAKE_DIRECTORY ${dst}/${relative_subdir})
+    execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink
+                    ${src}/${FILENAME}
+                    ${dst}/${FILENAME})
+  endforeach(FILENAME)
+endfunction()
