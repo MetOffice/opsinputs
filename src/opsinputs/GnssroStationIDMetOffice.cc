163992cddbdb7539acb9cbcc968a47e2e83140e7
Neill Bowler <45592137+neill-b@users.noreply.github.com>
2021-12-02 09:08:33 +0000

    Introduce obs function to create station_id for GNSS-RO (#54)
    * First changes to add new files.
    
    * Add new filter to yaml file, and add sat ID to the netcdf file.
    
    * Change copyright year and use oops::Log for output.
    
    * Changes suggested by Mike.
    
    Co-authored-by: mikecooke77 <michael.c.cooke@metoffice.gov.uk>

---
+++ src/opsinputs/GnssroStationIDMetOffice.cc
@@ -0,0 +1,102 @@
+/* -----------------------------------------------------------------------------
+ * (C) British Crown Copyright 2021 Met Office
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * -----------------------------------------------------------------------------
+ *
+ * Find the station ID for GNSS-RO observations used by the Met Office's DA
+ * system.
+ *
+ * Example yaml snippet:
+ *  - filter: Variable Assignment
+ *   assignments:
+ *   - name: station_id@MetaData
+ *     type: string
+ *     function:
+ *       name: GnssroStationIDMetOffice@StringObsFunction
+ * -----------------------------------------------------------------------------
+ */
+#include "opsinputs/GnssroStationIDMetOffice.h"
+
+#include <math.h>
+#include <string>
+#include <vector>
+
+#include "ioda/ObsDataVector.h"
+#include "oops/util/missingValues.h"
+#include "ufo/filters/Variable.h"
+
+namespace ufo {
+
+static ObsFunctionMaker<GnssroStationIDMetOffice>
+  makerGnssroStationIDMetOffice_("GnssroStationIDMetOffice");
+
+/* -----------------------------------------------------------------------------
+ * Specify that satellite identifier is noted as required by this function.
+ * -----------------------------------------------------------------------------
+ */
+GnssroStationIDMetOffice::GnssroStationIDMetOffice(const eckit::LocalConfiguration & conf)
+  : invars_() {
+  invars_ += Variable("MetaData/occulting_sat_id");
+}
+
+// -----------------------------------------------------------------------------
+
+GnssroStationIDMetOffice::~GnssroStationIDMetOffice() {}
+
+/* -----------------------------------------------------------------------------
+ * Perform the computation.  For each profile construct the station_id as a
+ * fixed length string.
+ * -----------------------------------------------------------------------------
+ */
+void GnssroStationIDMetOffice::compute(const ObsFilterData & in,
+                                       ioda::ObsDataVector<std::string> & out) const {
+  // Get the satellite identifier
+  const size_t nlocs = in.nlocs();
+  std::vector<int> satid;
+  in.get(Variable("MetaData/occulting_sat_id"), satid);
+
+  // Get the record number of each profile
+  std::vector<size_t> recordNumbers = out.space().recidx_all_recnums();
+  oops::Log::debug() << "Unique record numbers" << std::endl;
+  for (size_t record : recordNumbers) {
+    oops::Log::debug() << record << " ";
+  }
+  oops::Log::debug() << std::endl;
+
+  // For each profile, apply the function to each member
+  for (const size_t& iProfile : recordNumbers) {
+    const std::vector<size_t> & obsNumbers = out.space().recidx_vector(iProfile);
+    for (size_t jobs : obsNumbers) {
+      out[0][jobs] = stringFormat(satid[jobs], 4) +
+                     stringFormat(iProfile, 5) +
+                     stringFormat(jobs, 7);
+    }
+  }
+}
+
+/// Convert an integer to a fixed length string, checking whether the string is
+/// too long for the output
+std::string GnssroStationIDMetOffice::stringFormat
+                            (const size_t input, const size_t width) const {
+  std::stringstream returnStream;
+  if (input >= pow(10, width)) {
+    oops::Log::warning() << "stringFormat: " << input << " is too wide for a "
+                         << width << " string" << std::endl;
+    returnStream << std::setw(width) << std::setfill('0');
+  } else {
+    returnStream << std::setw(width) << std::setfill('0') << input;
+  }
+  return returnStream.str();
+}
+
+// -----------------------------------------------------------------------------
+
+const ufo::Variables & GnssroStationIDMetOffice::requiredVariables() const {
+  return invars_;
+}
+
+// -----------------------------------------------------------------------------
+
+}  // namespace ufo
---
+++ src/opsinputs/GnssroStationIDMetOffice.h
@@ -0,0 +1,42 @@
+/* -----------------------------------------------------------------------------
+ * (C) British Crown Copyright 2021 Met Office
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * -----------------------------------------------------------------------------
+ */
+
+#ifndef OPSINPUTS_GNSSROSTATIONIDMETOFFICE_H_
+#define OPSINPUTS_GNSSROSTATIONIDMETOFFICE_H_
+
+#include <string>
+
+#include "ufo/filters/ObsFilterData.h"
+#include "ufo/filters/obsfunctions/ObsFunctionBase.h"
+#include "ufo/filters/Variables.h"
+
+namespace ufo {
+///
+/// \brief Function calculates the GNSS-RO station ID for GNSS-RO observations
+///        as a 16-character string of the form AAAABBBBBCCCCCCC where:
+///        AAAA is the satellite ID
+///        BBBBB is the profile number
+///        CCCCCCC is the observation number
+///
+class GnssroStationIDMetOffice : public ObsFunctionBase<std::string> {
+ public:
+  explicit GnssroStationIDMetOffice(const eckit::LocalConfiguration &);
+  ~GnssroStationIDMetOffice();
+
+  void compute(const ObsFilterData &,
+               ioda::ObsDataVector<std::string> &) const;
+  const ufo::Variables & requiredVariables() const;
+ private:
+  ufo::Variables invars_;
+  std::string stringFormat(const size_t, const size_t) const;
+};
+// -----------------------------------------------------------------------------
+
+}  // namespace ufo
+
+#endif  // OPSINPUTS_GNSSROSTATIONIDMETOFFICE_H_
