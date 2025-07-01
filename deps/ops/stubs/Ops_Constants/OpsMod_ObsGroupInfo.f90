67f28c2b32466f90438fd5de1fa18a0b0f67a481
brettcandy <brett.candy@metoffice.gov.uk>
2025-07-01 09:55:53 +0100

    updates for the rename

--- deps/ops/stubs/Ops_Constants/OpsMod_ObsGroupInfo.f90
+++ deps/ops/stubs/Ops_Constants/OpsMod_ObsGroupInfo.f90
@@ -78,7 +78,7 @@ INTEGER, PARAMETER :: ObsGroupAscatLSDA      =  66 ! => LSDA (ASCAT)
 INTEGER, PARAMETER :: ObsGroupMTGIRS         =  67 ! => MTG-IRS radiances
 INTEGER, PARAMETER :: ObsGroupFCIASR         =  68 ! => FCI all-sky radiances
 INTEGER, PARAMETER :: ObsGroupFCIClr         =  69 ! => FCI clear-sky radiances
-INTEGER, PARAMETER :: ObsGroupSternaMWS      =  70 ! => AWS & Sterna microwave radiances
+INTEGER, PARAMETER :: ObsGroupSternaMWR      =  70 ! => AWS & Sterna microwave radiances
 INTEGER, PARAMETER :: ObsGroupEPSMWS         =  71 ! => EPS SG MWS 
 INTEGER, PARAMETER :: max_obs_group_num      =  71 ! Number of observation groups
 
