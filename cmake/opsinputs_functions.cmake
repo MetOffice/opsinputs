0a45952a04b5e2b2abfe83a070790438f9155628
Matt Shin <matthew.shin@metoffice.gov.uk>
2026-08-18 09:49:14 +0100

    Launch fewer CMake subprocesses (#294)
    - Replace `execute_process(... -E create_symlink ...)`
      with `file(CREATE_LINK ... SYMBOLIC)`.
    - Bumps CMake version requirement to 3.26 to align with ops-um-jedi and
      mo-bundle.
    - Update copyright notices.

--- cmake/opsinputs_functions.cmake
+++ cmake/opsinputs_functions.cmake
@@ -1,4 +1,4 @@
-# (C) Crown Copyright 2021, the Met Office. All rights reserved.
+# (C) Crown Copyright Met Office. All rights reserved.
 #
 #
 # Syntax: CREATE_SYMLINKS(src dst)
@@ -7,16 +7,14 @@
 # - src: Source folder
 # - dst: Destination folder
 #
-# Creates symbolic links in the destination folder pointing to the specified files in the source
-# folder, preserving subfolder hierarchy.
+# Creates symbolic links in the destination folder pointing to the specified
+# files in the source folder, preserving subfolder hierarchy.
 function(CREATE_SYMLINKS src dst)
   file(MAKE_DIRECTORY ${dst})
   foreach(FILENAME ${ARGN})
     get_filename_component(absolute_subdir ${src}/${FILENAME} DIRECTORY)
     file(RELATIVE_PATH relative_subdir ${src} ${absolute_subdir})
     file(MAKE_DIRECTORY ${dst}/${relative_subdir})
-    execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink
-                    ${src}/${FILENAME}
-                    ${dst}/${FILENAME})
+    file(CREATE_LINK ${src}/${FILENAME} ${dst}/${FILENAME} SYMBOLIC)
   endforeach()
 endfunction()
