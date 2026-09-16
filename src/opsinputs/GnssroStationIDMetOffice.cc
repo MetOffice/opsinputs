29fd4774d371084a069d7fc68d92f3336eefdca5
Matt Shin <matthew.shin@metoffice.gov.uk>
2026-09-16 08:47:22 +0100

    Fix cpplint failure following #298
    Also add cpplint to regular lint GH Actions.

--- src/opsinputs/GnssroStationIDMetOffice.cc
+++ src/opsinputs/GnssroStationIDMetOffice.cc
@@ -24,8 +24,8 @@
 
 #include "ioda/ObsDataVector.h"
 #include "ioda/ObsSpace.h"
-#include "oops/util/missingValues.h"
 #include "oops/util/Logger.h"
+#include "oops/util/missingValues.h"
 #include "ufo/filters/Variable.h"
 
 namespace ufo {
